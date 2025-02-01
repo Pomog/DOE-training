552098acc7312bde10693439ef0a3652b40ab3cc
Yurii Panasuik <thoryur@gmail.com>
2025-02-02 01:42:16 +0200

    linear model solvent

--- b2.R
+++ b2.R
@@ -26,20 +26,51 @@ b5 <- as.coded.data(b5,
 str(b5)
 
 #model.Yield_percent <- rsm(Yield_percent ~ FO(volume,eq) + TWI(volume,eq) + PQ(volume,eq), data = b5)
-model.Yield_percent <- rsm(Yield_percent ~ FO(volume), data = b5)
+
+b5_clean <- b5[-9, ]  # Remove 9th row
+#model.Yield_percent <- rsm(Yield_percent ~ FO(volume,eq) + TWI(volume,eq) + PQ(volume,eq), data = b5_clean)
+
+#model.Yield_percent <- rsm(Yield_percent ~ FO(volume,eq), data = b5_clean)
+model.Yield_percent <- rsm(Yield_percent ~ FO(volume), data = b5_clean)
 
 # Check model summary
 summary(model.Yield_percent)
 
 par(mfrow=c(2,3))
 
-plot(b5$Yield_percent, model.Yield_percent$residuals, 
+plot(b5_clean$Yield_percent, model.Yield_percent$residuals, 
      xlab = "Yield_percent (experimental)", ylab = "residuals") + 
               abline(h=0, col="grey")
 
-plot(b5$Solvent_V, model.Yield_percent$residuals, 
+plot(b5_clean$Solvent_V, model.Yield_percent$residuals, 
      xlab = "Solvent_V", ylab = "residuals") + 
   abline(h=0, col="grey")
+
+
+qqnorm(model.Yield_percent$residuals)  # Creates the Q-Q plot
+qqline(model.Yield_percent$residuals, col = "red")  # Adds reference line
+
+install.packages("lmtest")  # Install if you haven't
+library(lmtest)
+bptest(model.Yield_percent)  # Perform Breusch-Pagan test for heteroscedasticity
+
+# plot the relationship between Yield_percent and volume
+min_volume <- min(b5_clean$volume)
+max_yield <- predict(model.Yield_percent, newdata = data.frame(volume = min_volume))
+# Create a sequence of volume values
+volume_seq <- seq(min(b5_clean$volume), max(b5_clean$volume), length.out = 100)
+
+# Predict yield for each volume
+predicted_yield <- predict(model.Yield_percent, newdata = data.frame(volume = volume_seq))
+
+# Plot the regression line
+plot(b5_clean$volume, b5_clean$Yield_percent, 
+     xlab = "Volume", ylab = "Yield (%)", 
+     main = "Yield vs. Volume", pch = 16, col = "blue")
+
+# Add regression line
+lines(volume_seq, predicted_yield, col = "red", lwd = 2)
+
 
 plot(b5$Acid_eq, model.Yield_percent$residuals, 
      xlab = "Acid_eq", ylab = "residuals") + 
